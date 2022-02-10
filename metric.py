f196e7042e41f240131994c4f896eca0b3a6577a
kostenkohanna <99220470+kostenkohanna@users.noreply.github.com>
2022-02-10 18:31:54 +0300

    indicator color

--- metric.py
+++ metric.py
@@ -4,3 +4,8 @@ col1, col2, col3 = st.columns(3)
 col1.metric("Temperature", "70 °F", "1.2 °F")
 col2.metric("Wind", "9 mph", "-8%")
 col3.metric("Humidity", "86%", "4%")
+st.metric(label="Gas price", value=4, delta=-0.5,
+     delta_color="inverse")
+
+st.metric(label="Active developers", value=123, delta=123,
+     delta_color="off")
